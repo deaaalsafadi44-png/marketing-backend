347e2674d90d1a6df440d9d4f0b4243ee029362d
deaaalsafadi44-png <YOUR_EMAIL@example.com>
2025-12-18 05:28:47 +0300

    test deliverables route

--- routes/deliverables.routes.js
+++ routes/deliverables.routes.js
@@ -1,20 +1,28 @@
 const express = require("express");
 const authenticateToken = require("../middlewares/authenticateToken");
 const deliverablesController = require("../controllers/deliverables.controller");
+const upload = require("../middlewares/upload");
 
 const router = express.Router();
 
-// ✅ GET /deliverables
+/**
+ * GET /deliverables
+ * Used by Submissions page
+ */
 router.get(
   "/",
   authenticateToken,
   deliverablesController.getAllDeliverables
 );
 
-// ✅ POST /deliverables
+/**
+ * POST /deliverables
+ * Submit task deliverables
+ */
 router.post(
   "/",
   authenticateToken,
+  upload.array("files", 10),
   deliverablesController.createDeliverable
 );
 
