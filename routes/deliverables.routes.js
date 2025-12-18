b0231f0354572ff69140571e24e55a70f1467008
deaaalsafadi44-png <YOUR_EMAIL@example.com>
2025-12-18 04:53:33 +0300

    test deliverables route

--- routes/deliverables.routes.js
+++ routes/deliverables.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.json({ ok: true, message: "deliverables route works" });
+  res.send("DELIVERABLES ROUTE WORKS ✅");
 });
 
 module.exports = router;
